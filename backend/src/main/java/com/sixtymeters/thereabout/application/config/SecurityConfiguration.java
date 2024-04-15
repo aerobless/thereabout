e0f34237669a5f6f3663fa5d540510df5b3f11a4
Theo Winter <theo.winter@gmail.com>
2024-04-15 22:30:04 +0200

    configured swagger

---
+++ backend/src/main/java/com/sixtymeters/thereabout/application/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.sixtymeters.thereabout.application.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain oidcSecurityConfiguration(final HttpSecurity http) throws Exception {
+
+        log.warn("SECURITY CONFIGURATION LOADED");
+
+        http.authorizeHttpRequests(config ->{
+            config.anyRequest().permitAll();
+        });
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("dev")
+                .password(passwordEncoder().encode("dev"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

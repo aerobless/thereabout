3f939c958a48510bc8a3f1ef93796602961ced09
Theo Winter <theo.winter@gmail.com>
2024-04-28 14:36:01 +0200

    added heatmap example

--- frontend/src/app/modules/locationhistory/locationhistory.component.ts
+++ frontend/src/app/modules/locationhistory/locationhistory.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
-import {GoogleMap} from "@angular/google-maps";
+import {GoogleMap, MapHeatmapLayer} from "@angular/google-maps";
 
 @Component({
   selector: 'app-locationhistory',
   standalone: true,
   imports: [
-    GoogleMap
+    GoogleMap,
+    MapHeatmapLayer
   ],
   templateUrl: './locationhistory.component.html',
   styleUrl: './locationhistory.component.scss'
 })
 export class LocationhistoryComponent {
+
+  center = {lat: 37.774546, lng: -122.433523};
+  zoom = 12;
+  heatmapOptions = {radius: 5};
+  heatmapData = [
+    {lat: 37.782, lng: -122.447},
+    {lat: 37.782, lng: -122.445},
+    {lat: 37.782, lng: -122.443},
+    {lat: 37.782, lng: -122.441},
+    {lat: 37.782, lng: -122.439},
+    {lat: 37.782, lng: -122.437},
+    {lat: 37.782, lng: -122.435},
+    {lat: 37.785, lng: -122.447},
+    {lat: 37.785, lng: -122.445},
+    {lat: 37.785, lng: -122.443},
+    {lat: 37.785, lng: -122.441},
+    {lat: 37.785, lng: -122.439},
+    {lat: 37.785, lng: -122.437},
+    {lat: 37.785, lng: -122.435}
+  ];
 
 }

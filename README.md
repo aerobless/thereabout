40aa21cee1d1e91344dfac9d5c433454eeb03ee0
Theo Winter <theo.winter@gmail.com>
2024-05-08 21:18:43 +0200

    added ability to highlight a given datapoint as marker on the map, improved header

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # thereabout
 Thereabout is a self-hosted replacement for Google Location History. It can import your existing Google Location History and visualise it as a heatmap on Google Maps.
 
-![Thereabout UI](/documentation/img/v4.png)
+![Thereabout UI](/documentation/img/v5.png)
 
 ## Features
 + Load existing Google Location History ("Records.json") when exported via [Google Takeout](https://takeout.google.com).
@@ -10,19 +10,23 @@ Thereabout is a self-hosted replacement for Google Location History. It can impo
 + Visualise your location history as a heatmap on Google Maps
   + Filter by date range: custom, YTD, 1 year, 5 years, full history
 + Visualise a specific day as a polygon line on Google Maps
-  + Locate the first entry on the map
+  + Locate the first or any entry on the map
+  + List all location entries for the given day and show the selected entry as a marker on the map
   + Open Google Photos on the selected day
 + Geocoding: Search for any location on Google Maps
++ Endpoint to add new location entries continuously , e.g. via Home Assistant
 + Deploy the application via Docker Compose
 + GitHub Build Pipeline
 
 ## Roadmap
 + Add detailed geolocation via Google Maps & persist results in DB (verify how many requests are free)
 + Day overview page
-  + Show trip as connected lines instead of just heatmap
   + Integrate Google Photos API (low prio)
   + Weather
   + Major locations visited
+  + Delete location entry (high prio)
+  + Add location entry (low prio)
+  + Update location entry (low prio)
 + Endpoint to allow recording new location data via Home Assistant or similar
 + Statistics: number of countries visited, km travelled, ...
 + Security: User Login & secure endpoints
@@ -33,6 +37,7 @@ Thereabout is a self-hosted replacement for Google Location History. It can impo
 
 ## Bugs
 + It's possible to trigger multiple imports resulting in duplicated records
++ Timezones are weird & annoying as always... need to decide how to deal with viewing data from different time zones
 
 ## Why
 Google sadly has decided to discontinue Google Location History / Timeline for the web as you can read [here](https://support.google.com/maps/answer/14169818?visit_id=638499772171143198-2056154066&p=maps_odlh&rd=1). It's still available on device, but in my opinion it's fairly clunky to interact with it on a small screen and secondly it is also no longer possible to easily export the local location history. Especially being unable to export my location data is a no-go for me, as Google has been known to kill various projects/features.. see the [Google graveyard](https://killedbygoogle.com/). Since there is no good alternative that does what I want I've decided to develop my own self-hosted location history.
@@ -44,6 +49,10 @@ a Google Maps API Key to load the Map. Google may be able to track your usage of
 Please beware that Thereabout is not intended to be run on the public internet. Either run it fully locally or behind a 
 authorization server such as [Authelia](https://www.authelia.com/). Thereabout itself does not have a login solution at this
 time and is not intended as a multi-tenant application. Any user with access can see all data, including your Google Maps API key.
+
+## How to continue adding data
+See the Swagger UI for the API documentation under `http://localhost:9050/swagger-ui/index.html`. There is an endpoint to
+add new location data. You can use this endpoint to add new location data from Home Assistant with Node RED or some other automation tool.
 
 ## Usage
 1. Clone the repository or download the [docker-compose.yml](https://github.com/aerobless/thereabout/blob/main/docker-compose.yaml) file
--- frontend/src/app/modules/locationhistory/locationhistory.component.ts
+++ frontend/src/app/modules/locationhistory/locationhistory.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {
-  GoogleMap, MapAdvancedMarker,
-  MapGeocoder,
-  MapHeatmapLayer,
-  MapMarker,
-  MapMarkerClusterer,
-  MapPolyline
+    GoogleMap, MapAdvancedMarker,
+    MapGeocoder,
+    MapHeatmapLayer,
+    MapMarker,
+    MapMarkerClusterer,
+    MapPolyline
 } from "@angular/google-maps";
 import {LocationHistoryEntry, LocationService} from "../../../../generated/backend-api/thereabout";
 import {ToolbarModule} from "primeng/toolbar";
@@ -20,173 +20,184 @@ import {PanelModule} from "primeng/panel";
 import {NgIf} from "@angular/common";
 import {FloatLabelModule} from "primeng/floatlabel";
 import QuickFilterDateCombo from "./quick-filter-date-combo";
-import {TableModule} from "primeng/table";
+import {TableModule, TableRowSelectEvent} from "primeng/table";
 
 
 @Component({
-  selector: 'app-locationhistory',
-  standalone: true,
-  imports: [
-    GoogleMap,
-    MapHeatmapLayer,
-    ToolbarModule,
-    InputTextModule,
-    CardModule,
-    IconFieldModule,
-    InputIconModule,
-    FormsModule,
-    ButtonModule,
-    CalendarModule,
-    PanelModule,
-    NgIf,
-    MapPolyline,
-    MapMarker,
-    MapMarkerClusterer,
-    MapAdvancedMarker,
-    FloatLabelModule,
-    TableModule,
-  ],
-  templateUrl: './locationhistory.component.html',
-  styleUrl: './locationhistory.component.scss'
+    selector: 'app-locationhistory',
+    standalone: true,
+    imports: [
+        GoogleMap,
+        MapHeatmapLayer,
+        ToolbarModule,
+        InputTextModule,
+        CardModule,
+        IconFieldModule,
+        InputIconModule,
+        FormsModule,
+        ButtonModule,
+        CalendarModule,
+        PanelModule,
+        NgIf,
+        MapPolyline,
+        MapMarker,
+        MapMarkerClusterer,
+        MapAdvancedMarker,
+        FloatLabelModule,
+        TableModule,
+    ],
+    templateUrl: './locationhistory.component.html',
+    styleUrl: './locationhistory.component.scss'
 })
-export class LocationhistoryComponent implements OnInit{
+export class LocationhistoryComponent implements OnInit {
 
-  // Map configuration
-  center = {lat: 47.3919661, lng: 8.3};
-  zoom = 4;
-  searchValue: string = '';
+    // Map configuration
+    center = {lat: 47.3919661, lng: 8.3};
+    zoom = 4;
+    searchValue: string = '';
 
-  // Heatmap
-  heatmapOptions = {radius: 8, maxIntensity: 2};
-  heatmapData: { lng: number; lat: number }[] = [];
-  fromDate: Date = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
-  toDate: Date = new Date();
+    // Heatmap
+    heatmapOptions = {radius: 8, maxIntensity: 2};
+    heatmapData: { lng: number; lat: number }[] = [];
+    fromDate: Date = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+    toDate: Date = new Date();
 
-  // Day view
-  dayViewData: { lng: number; lat: number }[] = [];
-  dayViewDataFull: Array<LocationHistoryEntry> = [];
-  exactDate: Date = new Date();
-  @ViewChild('exactDateCalendarInput')
-  private exactDateCalendarInput: any;
+    // Day view
+    dayViewData: { lng: number; lat: number }[] = [];
+    dayViewDataFull: Array<LocationHistoryEntry> = [];
+    exactDate: Date = new Date();
+    @ViewChild('exactDateCalendarInput')
+    private exactDateCalendarInput: any;
 
-  selectedLocationEntry: any;
+    selectedLocationEntry: LocationHistoryEntry | undefined = undefined;
 
-  constructor(private readonly locationService: LocationService, private readonly geocodeService: MapGeocoder) {
-  }
+    constructor(private readonly locationService: LocationService, private readonly geocodeService: MapGeocoder) {
+    }
 
-  ngOnInit(){
-    this.loadHeatmapData();
-    this.loadDayViewData();
-  }
+    ngOnInit() {
+        this.loadHeatmapData();
+        this.loadDayViewData();
+    }
 
-  loadHeatmapData(){
-    if(!this.fromDate || !this.toDate) return;
-    this.locationService.getSparseLocations(this.dateToString(this.fromDate), this.dateToString(this.toDate)).subscribe(locations => {
-      this.heatmapData = locations.map(location => {
-        return {lat: location.latitude, lng: location.longitude}
-      });
-    });
-  }
+    loadHeatmapData() {
+        if (!this.fromDate || !this.toDate) return;
+        this.locationService.getSparseLocations(this.dateToString(this.fromDate), this.dateToString(this.toDate)).subscribe(locations => {
+            this.heatmapData = locations.map(location => {
+                return {lat: location.latitude, lng: location.longitude}
+            });
+        });
+    }
 
-  loadDayViewData(){
-    if(!this.exactDate) return;
-    this.locationService.getLocations(this.dateToString(this.exactDate), this.dateToString(this.exactDate)).subscribe(locations => {
-      this.dayViewDataFull = locations;
-      this.dayViewData = locations.map(location => {
-        return {lat: location.latitude, lng: location.longitude}
-      });
-    });
-  }
+    loadDayViewData() {
+        if (!this.exactDate) return;
+        this.locationService.getLocations(this.dateToString(this.exactDate), this.dateToString(this.exactDate)).subscribe(locations => {
+            this.dayViewDataFull = locations;
+            this.dayViewData = locations.map(location => {
+                return {lat: location.latitude, lng: location.longitude}
+            });
+        });
+    }
 
-  geocodeAddress($event: KeyboardEvent){
-    if($event.key == 'Enter'){
-      this.geocodeService.geocode({address: this.searchValue}).subscribe(result => {
-        if(result.status == 'OK'){
-          let location = result.results[0].geometry.location;
-          this.center = {lat: location.lat(), lng: location.lng()};
-          this.applyZoom(11);
+    geocodeAddress($event: KeyboardEvent) {
+        if ($event.key == 'Enter') {
+            this.geocodeService.geocode({address: this.searchValue}).subscribe(result => {
+                if (result.status == 'OK') {
+                    let location = result.results[0].geometry.location;
+                    this.center = {lat: location.lat(), lng: location.lng()};
+                    this.applyZoom(11);
+                }
+            });
         }
-      });
     }
-  }
 
-  dateToString(date: Date){
-    const year = date.getFullYear().toString().padStart(4, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
+    dateToString(date: Date) {
+        const year = date.getFullYear().toString().padStart(4, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
 
-    return `${year}-${month}-${day}`;
-  }
+        return `${year}-${month}-${day}`;
+    }
 
-  resetSearch() {
-    this.searchValue = '';
-  }
+    resetSearch() {
+        this.searchValue = '';
+    }
 
-  decrementDate() {
-    this.exactDate.setDate(this.exactDate.getDate() - 1);
-    this.exactDateCalendarInput.updateInputfield();
-    this.loadDayViewData();
-  }
+    decrementDate() {
+        this.exactDate.setDate(this.exactDate.getDate() - 1);
+        this.exactDateCalendarInput.updateInputfield();
+        this.loadDayViewData();
+    }
 
-  incrementDate() {
-    this.exactDate.setDate(this.exactDate.getDate() + 1);
-    this.exactDateCalendarInput.updateInputfield();
-    this.loadDayViewData();
-  }
+    incrementDate() {
+        this.exactDate.setDate(this.exactDate.getDate() + 1);
+        this.exactDateCalendarInput.updateInputfield();
+        this.loadDayViewData();
+    }
 
-  setQuickFilterForHeatmap(quickFilterDateOption: QuickFilterDateCombo){
-    switch (quickFilterDateOption) {
-        case QuickFilterDateCombo.YTD:
-          this.fromDate = new Date(new Date().getFullYear(), 0, 1);
-          this.toDate = new Date();
-          break;
-        case QuickFilterDateCombo.ONE_YEAR:
-          this.fromDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
-          this.toDate = new Date();
-          break;
-        case QuickFilterDateCombo.FIVE_YEARS:
-          this.fromDate = new Date(new Date().setFullYear(new Date().getFullYear() - 5));
-          this.toDate = new Date();
-          break;
-        case QuickFilterDateCombo.FULL_HISTORY:
-          this.fromDate = new Date(new Date().setFullYear(new Date().getFullYear() - 50));
-          this.toDate = new Date();
-          break;
+    setQuickFilterForHeatmap(quickFilterDateOption: QuickFilterDateCombo) {
+        switch (quickFilterDateOption) {
+            case QuickFilterDateCombo.YTD:
+                this.fromDate = new Date(new Date().getFullYear(), 0, 1);
+                this.toDate = new Date();
+                break;
+            case QuickFilterDateCombo.ONE_YEAR:
+                this.fromDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+                this.toDate = new Date();
+                break;
+            case QuickFilterDateCombo.FIVE_YEARS:
+                this.fromDate = new Date(new Date().setFullYear(new Date().getFullYear() - 5));
+                this.toDate = new Date();
+                break;
+            case QuickFilterDateCombo.FULL_HISTORY:
+                this.fromDate = new Date(new Date().setFullYear(new Date().getFullYear() - 50));
+                this.toDate = new Date();
+                break;
+
+        }
+        this.loadHeatmapData();
+    }
+
+    protected readonly QuickFilterDateCombo = QuickFilterDateCombo;
+
+    locateDayView() {
+        if (this.selectedLocationEntry) {
+            this.center = {lat: this.selectedLocationEntry.latitude, lng: this.selectedLocationEntry.longitude};
+            this.applyZoom(16);
+        } else {
+            if (this.dayViewData.length == 0) return;
+            this.center = this.dayViewData[0];
+            this.applyZoom(11);
+        }
+    }
+
+    setTodayForDayView() {
+        this.exactDate = new Date();
+    }
+
+    private applyZoom(zoom: number) {
+        if (this.zoom == zoom) {
+            this.zoom = zoom + 0.1;
+        } else {
+            this.zoom = zoom;
+        }
+    }
+
+    openGooglePhotos() {
+        window.open("https://photos.google.com/search/" + this.dateToString(this.exactDate), "_blank");
+    }
+
+    convertToLocalTime(date: string) {
+        return new Date(date).toLocaleTimeString().slice(0, 5);
+    }
+
+    shortCoordinates(lat: number, lng: number) {
+        return `${lat.toFixed(4)}, ${lng.toFixed(4)}`;
 
     }
-    this.loadHeatmapData();
-  }
 
-  protected readonly QuickFilterDateCombo = QuickFilterDateCombo;
+    onRowSelect($event: TableRowSelectEvent) {
 
-  locateDayView() {
-    if(this.dayViewData.length == 0) return;
-    this.center = this.dayViewData[0];
-    this.applyZoom(11);
-  }
-
-  setTodayForDayView(){
-    this.exactDate = new Date();
-  }
-
-  private applyZoom(zoom: number) {
-    if (this.zoom == zoom) {
-      this.zoom = zoom + 0.1;
-    } else {
-      this.zoom = zoom;
     }
-  }
 
-  openGooglePhotos() {
-    window.open("https://photos.google.com/search/"+this.dateToString(this.exactDate), "_blank");
-  }
-
-  convertToLocalTime(date: string){
-    return new Date(date).toLocaleTimeString().slice(0, 5);
-  }
-
-  shortCoordinates(lat: number, lng: number){
-    return `${lat.toFixed(4)}, ${lng.toFixed(4)}`;
-
-  }
+    protected readonly google = google;
 }
